4f308daa47018f94835d7850560ff9861fb21ce9
sologuboved <sologuboved@gmail.com>
2019-04-19 21:10:16 +0300

    improved pid writer + updated url searcher

--- bot.py
+++ bot.py
@@ -8,14 +8,33 @@ from tkn import TOKEN
 
 
 def write_pid():
-    pid_fname = '{}_{}.pid'.format(os.path.splitext(os.path.basename(sys.argv[0]))[0], str(os.getpid()))
+    prefix = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+    previous_pid = find_previous_pid(prefix)
+    if previous_pid:
+        print("\nRemoving {}...".format(previous_pid))
+        os.remove(previous_pid)
+    pid_fname = '{}_{}.pid'.format(prefix, str(os.getpid()))
+    print("Writing {}\n".format(pid_fname))
     with open(pid_fname, 'w') as handler:
         handler.write(str())
     return pid_fname
 
 
+def delete_pid(pid_fname):
+    try:
+        os.remove(pid_fname)
+    except FileNotFoundError as e:
+        print(str(e))
+
+
+def find_previous_pid(prefix):
+    for fname in os.listdir('.'):
+        if re.fullmatch(r'{}_\d+\.pid'.format(prefix), fname):
+            return fname
+
+
 def get_direct_link(page_link):
-    return re.findall(r'"src":"(https://.+?\.jpg)"', str(requests.get(page_link).content))[0]
+    return re.findall(r'{"src":"(.*?)","', str(requests.get(page_link).content))[-1]
 
 
 def send_direct_link(bot, update):
